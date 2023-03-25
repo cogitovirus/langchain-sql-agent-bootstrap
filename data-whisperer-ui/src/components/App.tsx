51bff24abe47c8da641d28a186063813eecd3f90
Wiktor Zolnierowicz <w.zolnierowicz@gmail.com>
2023-03-25 18:15:11 +0100

    checkpoint

--- data-whisperer-ui/src/components/App.tsx
+++ data-whisperer-ui/src/components/App.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import ChatComponent from "./ChatComponent";
 import DBViewComponent from "./DBViewComponent";
+import SideDrawer from './SideDrawer';
+
 
 const App: React.FC = () => {
 
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  const toggleSideDrawer = () => {
+    setSideDrawerOpen((prevState) => !prevState);
+  };
+
   return (
+
     <div className="container">
+      {/* <div>
+        <button onClick={toggleSideDrawer}>Toggle Side Drawer</button>
+        <SideDrawer show={sideDrawerOpen} />
+      </div> */}
       <div className="left">
         <ChatComponent />
       </div>
---
+++ data-whisperer-ui/src/components/SideDrawer.tsx
@@ -0,0 +1,30 @@
+// src/components/SideDrawer.tsx
+
+import React from 'react';
+import '../styles/SideDrawer.css';
+
+interface SideDrawerProps {
+  show: boolean;
+}
+
+const SideDrawer: React.FC<SideDrawerProps> = ({ show }) => {
+  const drawerClasses = show ? 'side-drawer open' : 'side-drawer';
+
+  return (
+    <nav className={drawerClasses}>
+      <ul>
+        <li>
+          <a href="/">Link 1</a>
+        </li>
+        <li>
+          <a href="/">Link 2</a>
+        </li>
+        <li>
+          <a href="/">Link 3</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideDrawer;
